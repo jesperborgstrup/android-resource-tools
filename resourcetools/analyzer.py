2e38dd87db58ddfa79b37d8b906a392e390b277a
Jesper Borgstrup <jesper@borgstrup.dk>
2013-08-22 10:02:12 +0200

    Initial commit

---
+++ resourcetools/analyzer.py
@@ -0,0 +1,17 @@
+'''
+Created on 22/08/2013
+
+@author: Jesper
+'''
+
+class ResourceAnalyzer(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
